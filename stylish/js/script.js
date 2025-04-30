21baaddae69133d3223b3d805944cae4c40db9ad
hyeon1000 <gd10835@gmail.com>
2025-04-30 15:25:39 +0900

    Update script.js

--- stylish/js/script.js
+++ stylish/js/script.js
@@ -1,10 +1,11 @@
 $(function() {
+    //nav 클릭 시,
     $('.nav_toggle').click(function() {
         $('.open_nav').stop().animate({ right: '0' },50);
         $('.popup').show()
     });
 
-
+//닫기 버튼 클릭 시,
     $('.close').click(function() {
        $('.popup').hide()
         $('.open_nav').animate({ right: '-120%' }, 100)
